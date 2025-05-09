e44f3393ed1471389ced4a8cd2a8167e22c72937
Sandeep Suresh <68272706+SandyTheAdventurer@users.noreply.github.com>
2025-05-09 09:57:09 +0530

    Label Encoded dataset

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 
 dataset = pd.read_csv("data.csv")
+
+dataset.drop(columns=['customerID'], inplace=True)
+
+encoder = LabelEncoder()
+for column in dataset.select_dtypes(include=['object']).columns:
+    dataset[column] = encoder.fit_transform(dataset[column])
